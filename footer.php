eaebe5c4449ed3898276aa0dbeeb34988c96c5c8
actiontwo <actiontwo@designveloper.com>
2015-03-06 19:06:15 +0700

    change footer link

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <footer class="footer">
     <div class="container">
         <ul class="row">
-            <li class="col-md-4"><a href="<?= get_option('facebook') ?>"><i class="iconsp-35-facebook"></i></a><a href="http://www.patragroup.com.au/privacy.html">Privacy Policy</a></li>
+            <li class="col-md-4"><a href="<?= get_option('facebook') ?>"><i class="iconsp-35-facebook"></i></a>
+              <a href="http://openseed.com.au/~patragroup/privacy-policy/">Privacy Policy</a></li>
             <li class="col-md-5">
                 <?php if (get_option('copyright')): ?>
                   <span><?= get_option('copyright') ?></span>
@@ -13,9 +14,9 @@
             </li>
             <li class="col-md-3">
                 <?php if (get_option('designBy')): ?>
-                <a href="www.openseed.com.au"><span><?= get_option('designBy') ?></span></a>
+                <a href="http://www.openseed.com.au" target="_blank"><span><?= get_option('designBy') ?></span></a>
                 <?php else : ?>
-                 <a href="www.openseed.com.au"><span>Designed by Openseed</span></a>
+                 <a href="http://www.openseed.com.au" target="_blank"><span>Designed by Openseed</span></a>
                 <?php endif ?>
 
             </li>
